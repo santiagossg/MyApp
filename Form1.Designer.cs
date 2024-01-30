0cd0132d943c013e9de77866be9137f6574bdd0f
santiagossg <118320186+santiagossg@users.noreply.github.com>
2024-01-30 13:25:07 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace MyApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rjToggleButton1 = new MyApp.RJCodeAdvance();
+            this.SuspendLayout();
+            // 
+            // rjToggleButton1
+            // 
+            this.rjToggleButton1.AutoSize = true;
+            this.rjToggleButton1.Checked = true;
+            this.rjToggleButton1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.rjToggleButton1.Location = new System.Drawing.Point(451, 67);
+            this.rjToggleButton1.MinimumSize = new System.Drawing.Size(45, 22);
+            this.rjToggleButton1.Name = "rjToggleButton1";
+            this.rjToggleButton1.Size = new System.Drawing.Size(124, 22);
+            this.rjToggleButton1.TabIndex = 0;
+            this.rjToggleButton1.Text = "rjToggleButton1";
+            this.rjToggleButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rjToggleButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private RJCodeAdvance rjToggleButton1;
+    }
+}
+
---
+++ RJCodeAdvance.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace MyApp
+{
+    public class RJCodeAdvance : CheckBox
+    {
+        private Color onBackColor = Color.MediumBlue;
+        private Color onToggleColor = Color.White; // Color del toggle cuando está encendido
+        private Color offBackColor = Color.Gray; // Color de fondo cuando está apagado
+        private Color offToggleColor = Color.White; // Color del toggle cuando está apagado
+
+        public RJCodeAdvance()
+        {
+            MinimumSize = new Size(45, 22);
+        }
+
+        public GraphicsPath GetFigurePath()
+        {
+            int arcSize = Height - 2;
+            Rectangle leftArc = new Rectangle(0, 0, arcSize, arcSize);
+            Rectangle rightArc = new Rectangle(Width - 2 - arcSize, 0, arcSize, arcSize);
+
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(leftArc, 90, 180);
+            path.AddArc(rightArc, 270, 180);
+            path.CloseFigure();
+
+            return path;
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            int toggleSize = Height - 5;
+            pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            pevent.Graphics.Clear(Parent.BackColor);
+
+            if (Checked) // Encendido
+            {
+                pevent.Graphics.FillPath(new SolidBrush(onBackColor), GetFigurePath());
+                pevent.Graphics.FillEllipse(new SolidBrush(onToggleColor),
+                    new Rectangle(Width - Height + 1, 2, toggleSize, toggleSize));
+            }
+            else // Apagado
+            {
+                pevent.Graphics.FillPath(new SolidBrush(offBackColor), GetFigurePath());
+                pevent.Graphics.FillEllipse(new SolidBrush(offToggleColor),
+                    new Rectangle(2, 2, toggleSize, toggleSize));
+            }
+        }
+    }
+}
+
